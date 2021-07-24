45a3fb2db4635b31a4bfaac0a041aac04124b68f
Jon Mockbee <mockbee1000@gmail.com>
2021-07-24 12:43:46 -0400

    using beautiful soup

---
+++ pyscrape.py
@@ -0,0 +1,9 @@
+import requests 
+# changing name to bs
+from bs4 import BeautifulSoup as bs
+
+awscert  = input ('input aws:')
+url = 'https://www.aws.training/certification' + awscert
+r = requests.get(url)
+soup = bs (r.content, 'html.parser')
+account = soup.find ()
